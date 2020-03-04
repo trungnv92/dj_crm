76b8f3f2d2724803fda009d0638ee6da0b742e13
trungcuibap <trungnv.vnimm@gmail.com>
2020-03-04 14:04:04 +0700

    edit file setting.py

--- crm/settings.py
+++ crm/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7(!f!(tux^4ofn$bq)j_z)!o6-m#q!$4#shs5bpamx*_uu^91)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['trungnv-dj-crm.herokuapp.com', '127.0.0.1']
 
